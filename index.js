04e372706727a1551e1b9f7956ddbb730a14b61d
Thomas Wilburn <news@thomaswilburn.net>
2018-05-11 12:09:23 -0700

    I like this init style better

--- index.js
+++ index.js
@@ -1,73 +1,69 @@
 var puppet = require("puppeteer");
 var fs = require("fs");
-var window;
 
-var startChrome = async function() {
-  var browser = await puppet.launch();
-  window = await browser.newPage();
+puppet.launch().then(async (browser) => {
+
+  var window = await browser.newPage();
   await window.viewport({ width: 1920, height: 1080 });
-};
 
-var lazyLoad = function() {
-  var lazyImages = [...document.querySelectorAll(".lazy")];
-  var loading = lazyImages.map(function(img) {
-    return new Promise(function(ok, fail) {
-      img.src = img.getAttribute("data-src");
-      img.classList.add("loaded");
-      // console.log(img.src)
-      img.addEventListener("load", ok);
+  var lazyLoad = function() {
+    var lazyImages = [...document.querySelectorAll(".lazy")];
+    var loading = lazyImages.map(function(img) {
+      return new Promise(function(ok, fail) {
+        img.src = img.getAttribute("data-src");
+        img.classList.add("loaded");
+        // console.log(img.src)
+        img.addEventListener("load", ok);
+      });
     });
-  });
-  var done = Promise.all(loading);
-  return done.then(() => new Promise(ok => setTimeout(ok, 10000)));
-};
+    var done = Promise.all(loading);
+    return done.then(() => new Promise(ok => setTimeout(ok, 10000)));
+  };
 
-var mkdir = function(path) {
-  if (fs.existsSync(path)) return;
-  fs.mkdirSync(path);
-};
+  var mkdir = function(path) {
+    if (fs.existsSync(path)) return;
+    fs.mkdirSync(path);
+  };
 
-var getPath = function() {
-  var now = new Date();
-  var year = now.getFullYear();
-  var month = now.getMonth().toString().padStart(2, "0");
-  var day = now.getDate().toString().padStart(2, "0");
-  var hour = now.getHours().toString().padStart(2, "0");
-  var folder = `${year}${month}${day}`;
-  var file = `${hour}.jpg`;
-  var path = folder + "/" + file;
-  return { folder, file, path };
-};
-
-var visit = async function() {
-  await window.goto("https://seattletimes.com", { waitUntil: "domcontentloaded" });
-  await window.evaluate(lazyLoad);
-  var { folder, path } = getPath();
-  mkdir(folder);
-  await window.screenshot({ width: 1440, path, fullPage: true });
-  console.log("Creating screenshot: ", path);
-};
-
-var wait = function(minutes) {
-  return new Promise(ok => setTimeout(ok, minutes * 60 * 1000));
-};
-
-var nextHour = function() {
-  return new Promise(ok => {
+  var getPath = function() {
     var now = new Date();
-    var later = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, 0);
-    var difference = later - now;
-    setTimeout(ok, difference);
-  });
-}
+    var year = now.getFullYear();
+    var month = now.getMonth().toString().padStart(2, "0");
+    var day = now.getDate().toString().padStart(2, "0");
+    var hour = now.getHours().toString().padStart(2, "0");
+    var folder = `${year}${month}${day}`;
+    var file = `${hour}.jpg`;
+    var path = folder + "/" + file;
+    return { folder, file, path };
+  };
 
-var shutter = async function() {
-  await visit();
-  await nextHour();
-  shutter();
-};
+  var visit = async function() {
+    await window.goto("https://seattletimes.com", { waitUntil: "domcontentloaded" });
+    await window.evaluate(lazyLoad);
+    var { folder, path } = getPath();
+    mkdir(folder);
+    await window.screenshot({ width: 1440, path, fullPage: true });
+    console.log("Creating screenshot: ", path);
+  };
 
-(async () => {
-  await startChrome();
+  var wait = function(minutes) {
+    return new Promise(ok => setTimeout(ok, minutes * 60 * 1000));
+  };
+
+  var nextHour = function() {
+    return new Promise(ok => {
+      var now = new Date();
+      var later = new Date(now.getFullYear(), now.getMonth(), now.getDate(), now.getHours() + 1, 0);
+      var difference = later - now;
+      setTimeout(ok, difference);
+    });
+  }
+
+  var shutter = async function() {
+    await visit();
+    await nextHour();
+    shutter();
+  };
+
   shutter();
-})();
+});
